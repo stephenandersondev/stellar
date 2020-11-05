d25a7270555d9f5263f283d5688ded88ba2db104
stephenandersondev <stephenandersondev@gmail.com>
2020-11-05 16:45:43 -0600

    finished backend

--- stellar-backend/app/models/resource.rb
+++ stellar-backend/app/models/resource.rb
@@ -1,4 +1,10 @@
 class Resource < ApplicationRecord
   belongs_to :project
   belongs_to :user
+
+
+  def get_apod
+    
+  end
+
 end
--- stellar-backend/app/models/user.rb
+++ stellar-backend/app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   has_many :messages
   has_many :resources 
   belongs_to :project
+
+  has_secure_password
 end
