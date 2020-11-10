7c7d6936fc14d1ee4e27c5e364f05365a4a9edf6
BenLooper <benlooper08@gmail.com>
2020-11-10 17:06:57 -0600

    adjusted add project navigation

--- stellar-frontend/src/containers/NewProject.js
+++ stellar-frontend/src/containers/NewProject.js
@@ -5,19 +5,21 @@ import { useHistory, withRouter } from 'react-router'
 
 
 function NewProject(props) {
+
     const history = useHistory();
 
-    const routeChange = () => {
-        let path = `/signup`;
+    const routeChange = (newPath) => {
+        let path = `/${newPath}`;
         history.push(path);
     }
+    
     return (
         <div className='home-screen' style={{
             background: `url(${props.apodImg}) no-repeat center center
         fixed`, backgroundRepeat:"no-repeat", backgroundSize:"cover", height:"100vh"}}>
         <Container align="center">
             <Image src={Logo} alt="Logo" height="250" width="auto" />
-            <Form onSubmit={(props.newProject)}>
+            <Form onSubmit={(props.newProject)} >
                 <Form.Group controlId="forTitle">
                     <Form.Label>Title</Form.Label>
                     <Form.Control type="text" placeholder="Title" required />
@@ -26,9 +28,9 @@ function NewProject(props) {
                     <Form.Label>Description</Form.Label>
                     <Form.Control type="text" placeholder="Description" required />
                 </Form.Group>
-                <Button variant="primary" type="submit">Add Project</Button>
+                <Button variant="primary" type="submit" >Add Project</Button>
             </Form>
-            <Button onClick={routeChange} className="mt-4" variant="secondary" type="button">User Sign Up</Button>
+            <Button onClick={()=>routeChange('signup')} className="mt-4" variant="secondary" type="button">User Sign Up</Button>
         </Container>
         </div>
     )
